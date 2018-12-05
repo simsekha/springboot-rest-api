fc0ecc455e49082ad0fa518ce8e8d9f7d1e93df1
hayri.simsek <simsekha@hotmail.com>
2018-12-05 20:59:14 +0300

    HATEOAS added

--- src/main/java/com/simsekonline/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/simsekonline/rest/webservices/restfulwebservices/user/UserController.java
@@ -6,6 +6,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,25 +30,31 @@ public class UserController {
 	}
 
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
-		User user= service.findOne(id);
-		if(user==null) {
+	public Resource<User> retrieveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		if (user == null) {
 			throw new UserNotFoundException("User not found " + id);
 		}
-		return user;
+
+		Resource<User> resource = new Resource<User>(user);
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+
+		return resource;
 	}
-	
-	@PostMapping ("/users")
+
+	@PostMapping("/users")
 	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
-		User userCreated=service.save(user);
-		URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userCreated.getId()).toUri();
-		return ResponseEntity.created(location).build();		
+		User userCreated = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(userCreated.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
-		User user= service.deleteByID(id);
-		if(user==null) {
+		User user = service.deleteByID(id);
+		if (user == null) {
 			throw new UserNotFoundException("User not found " + id);
 		}
 	}
