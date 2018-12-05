bf4ce0acac226e9ce2dc1702dc6d4e5ff93b9c33
hayri.simsek <simsekha@hotmail.com>
2018-12-05 19:59:07 +0300

    global custom error handling added

---
+++ src/main/java/com/simsekonline/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
@@ -0,0 +1,48 @@
+package com.simsekonline.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+public class ExceptionResponse {
+	private Date timestamp;
+	private String message;
+	private String details;
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "ExceptionResponse [timestamp=" + timestamp + ", message=" + message + ", details=" + details + "]";
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+
+
+}
--- src/main/java/com/simsekonline/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/simsekonline/rest/webservices/restfulwebservices/user/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-public class UserResource {
+public class UserController {
 	@Autowired
 	private UserDaoService service;
 
