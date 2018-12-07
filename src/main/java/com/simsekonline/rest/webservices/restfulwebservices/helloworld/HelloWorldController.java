81bcec1831f32d55782c43d0186d3b3886afa190
hayri.simsek <simsekha@hotmail.com>
2018-12-07 12:49:23 +0300

    internationalized first step

--- src/main/java/com/simsekonline/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/simsekonline/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.simsekonline.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@RequestMapping(path="/hello-world")
 	public String HelloWorld() {
 		return "Hello world";
@@ -21,6 +29,11 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean/{name}")
 	public HelloWorldBean HelloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean("Hello world from "+name);
+	}
+	
+	@RequestMapping(path="/hello-world-internationalized")
+	public String HelloWorldInternationalized(@RequestHeader(name="Accept-Language",required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null,locale);
 	}
 	
 }
